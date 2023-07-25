4284377ed53ac4f5c83c47fc84070a5e5a4c48e3
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-24 21:52:39 -0400

    Boolens dataTypes

---
+++ Data_Types/boolens.dart
@@ -0,0 +1,17 @@
+/*
+In Dart, boolean holds either true or false value. 
+You can write the bool keyword to define the boolean data type. 
+You can use boolean if the answer is true or false. 
+Consider the answer to the following questions:
+      Are you married?
+      Is the door open?
+      Does a cat fly?
+      Is the traffic light green?
+      Are you older than your father?
+These all are yes/no questions. Its a good idea to store them in boolean.
+ */
+
+void main() {
+  bool isMarried = true;
+  print('khaled marriage status $isMarried');
+}

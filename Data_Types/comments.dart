ef7b1917db5bb9b8aa2ad9225f6bbe1618261b73
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-25 05:21:00 -0400

    Comments DataTypes

---
+++ Data_Types/comments.dart
@@ -0,0 +1,17 @@
+/*
+Comments are the set of statements that are ignored by the dart compiler during program execution. 
+They are used to explain the code so that you or other people can understand it easily.
+
+Types Of Comments
+Single-Line Comment: For commenting on a single line of code. E.g. // This is a single-line comment.
+Multi-Line Comment: For commenting on multiple lines of code. E.g. /* This is a multi-line comment. */
+Documentation Comment: For generating documentation or reference for a project/software package. E.g. /// This is a documentation comment
+
+ */
+
+void main() {
+/*
+This is a multi-line comment.
+*/
+  print("Welcome to Khaled dart Respository.");
+}

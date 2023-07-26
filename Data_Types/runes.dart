1cd5442e4468f16010466cf2e4be9b77b18a3d0c
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-26 07:54:09 +0300

    Runes in dart

---
+++ Data_Types/runes.dart
@@ -0,0 +1,12 @@
+/*
+It is the sequence of UTF - 16 Unicode characters where Dart Runes are the sequence UTF - 32 Unicode code points.
+It is a UTF-32 string which is used to print the special symbol. 
+ */
+
+void main() {
+  var heart_rune = '\u2665';
+  var theta_rune = '\u{1f600}';
+
+  print(heart_rune);
+  print(theta_rune);
+}

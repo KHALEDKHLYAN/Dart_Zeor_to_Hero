83c50af6031ef48e2a3faa682f6f4cac6f7686eb
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-27 04:51:10 +0300

    Questions/answers in dart

--- Data_Types/Questions/q7.dart
+++ Data_Types/Questions/q7.dart
@@ -16,3 +16,16 @@ void main() {
   print("Quotient: $Quotient");
   print("Remainder: $Remainder");
 }
+
+/*
+In Dart, the term "quotient" refers to the result of integer division, while "remainder" refers to the remainder left over after performing the division. 
+Both quotient and remainder are obtained when using the integer division operator (~), also known as the "truncate division" or "floor division" operator.
+The formula for integer division is:
+        dividend ~/ divisor
+where dividend is the number being divided and divisor is the number you are dividing by.
+        Quotient: The quotient is the whole number part of the result obtained from the division, excluding any fractional part. 
+        In Dart, you can calculate the quotient using the ~/ operator.
+
+        Remainder: The remainder is the part of the dividend that is left over after performing the division. 
+        In Dart, you can obtain the remainder using the % operator.
+ */

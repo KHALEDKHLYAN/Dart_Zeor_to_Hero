e9562902b2950c7e848599ed6ca87c79fd3236c2
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-29 04:51:10 +0300

    Operators dart

--- operetors.dart
+++ operetors.dart
@@ -44,4 +44,16 @@ void main() {
   print("The division is $div");
   print("The interger division is $div2");
   print("The modulus is $mod");
+
+  //Assignment Operators
+//It is used to assign some values to variables. Here, we are assigning 24 to the age variable.
+  int age = 24;
+  age += 1; //here means age = age + 1
+  print("After Addition age is $age");
+  age -= 1; // here means age = age -1;
+  print("After SUbtracting age is $age");
+  age *= 2; // here means age = age *2
+  print("After Multiplying age $age");
+  age ~/= 2; // here means age = age / 2
+  print("After Division age is $age");
 }

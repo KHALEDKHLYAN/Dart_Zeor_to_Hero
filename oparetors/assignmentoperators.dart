260a8da2fcb259882c27880518fd903373e9fe0b
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-30 04:02:55 +0300

    commit oparators

---
+++ oparetors/assignmentoperators.dart
@@ -0,0 +1,17 @@
+/*
+              Assignment Operators
+It is used to assign some values to variables. Here, we are assigning 24 to the age variable.
+
+*/
+
+void main() {
+  int age = 24;
+  age += 1; //here means age = age + 1
+  print("After Addition age is $age");
+  age -= 1; // here means age = age -1
+  print("After SUbtracting age is $age");
+  age *= 2; // here means age = age *2
+  print("After Multiplying age $age");
+  age ~/= 2; // here means age = age / 2
+  print("After Division age is $age");
+}
---
+++ oparetors/logicaloperators.dart
@@ -0,0 +1,20 @@
+/*
+Logical Operators
+It is used to compare values.
+      Examples of Logical Operators are.
+          && --->  (AND) Returns true if all Conditions are true
+          || --->   (OR) Returns true if one condition is true
+          ! --->    (NOT) Returns false if the result is true and vice versa.
+ */
+
+void main() {
+  int stdId = 123;
+  int stdPin = 456;
+
+  print((stdId == 123) && (stdPin == 456));
+  print((stdId == 1223) && (stdPin == 456));
+  print((stdId == 123) || (stdPin == 456));
+  print((stdId == 1233) || (stdPin == 456));
+  print((stdId == 123) != (stdPin == 456));
+  print((stdId == 1223) != (stdPin == 456));
+}

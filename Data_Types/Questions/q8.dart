c5c8226e2021839225b2b0957301ec81c7db5e9b
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-30 05:28:05 +0300

    commit q8

--- Data_Types/Questions/q8.dart
+++ Data_Types/Questions/q8.dart
@@ -1,19 +1,25 @@
 //8. Write a program to swap two numbers.
 
-import 'dart:io';
+// import 'dart:io';
 
-main() {
+void main() {
   //WA Program to Swap Numbers Using Temporary Variable
-  var num1, num2, temp;
-  stdout.write("Enter first Number: ");
-  num1 = int.parse(stdin.readLineSync()!);
-  stdout.write("Enter Second Number: ");
-  num2 = int.parse(stdin.readLineSync()!);
+  // var num1, num2, temp;
+  // stdout.write("Enter first Number: ");
+  // num1 = int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter Second Number: ");
+  // num2 = int.parse(stdin.readLineSync()!);
 
-  temp = num1;
-  num1 = num2;
-  num2 = temp;
+  // temp = num1;
+  // num1 = num2;
+  // num2 = temp;
 
-  print("After Swaping first number is $num1");
-  print("After Swaping second number is $num2");
+  // print("After Swaping first number is $num1");
+  // print("After Swaping second number is $num2");
+
+  int a = 7, b = 3;
+
+  (a, b) == (b, a);
+
+  print('$a ... $b');
 }

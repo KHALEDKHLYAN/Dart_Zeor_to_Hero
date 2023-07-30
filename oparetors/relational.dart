6b8a3e551bd6a6a859050cbf9bf4c43da3d6253d
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-30 04:44:04 +0300

    commit oparators

---
+++ oparetors/relational.dart
@@ -0,0 +1,23 @@
+/*
+ALso Comparison Operators. They're Used to make Comparison
+ Examples 
+ > (Greater than)
+ < (Less than or equal to)
+ >= (Greator than or Equal to)
+ <= (Less than or Equal to)
+ == (Equal to)
+ != (Not Equal to)
+
+ */
+
+void main() {
+  int valu1 = 30;
+  int number = 15;
+
+  print(valu1 > number);
+  print(valu1 < number);
+  print(valu1 >= number);
+  print(valu1 <= number);
+  print(valu1 == number);
+  print(valu1 != number);
+}

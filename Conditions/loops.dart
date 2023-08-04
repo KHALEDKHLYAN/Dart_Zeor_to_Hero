a3192b3b6a218ca3015a3ac20b72e58e21959dc6
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-04 04:29:20 +0300

    loops

---
+++ Conditions/loops.dart
@@ -0,0 +1,16 @@
+/*
+Note: The primary purpose of all loops is to repeat a block of code.
+In Programming, loops are used to repeat a block of code until certain conditions are not completed. 
+For, e.g., if you want to print your name 100 times, then rather than typing print(“your name”); 100 times, you can use a loop.
+      There are different types of loop in Dart. They are:
+          For Loop
+          For Each Loop
+          While Loop
+          Do While Loop
+*/
+
+void main() {
+  for (int i = 1; i < 100; i++) {
+    print("$i John Doe");
+  }
+}

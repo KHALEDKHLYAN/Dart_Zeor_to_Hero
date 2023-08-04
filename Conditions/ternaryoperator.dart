8e0f85605d1b7c5e9ca054fd5280972b5bd7c10b
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-04 03:26:21 +0300

    terniary Operators

---
+++ Conditions/ternaryoperator.dart
@@ -0,0 +1,9 @@
+/*
+Ternary Operator
+The ternary operator is like if-else statement. 
+This is a one-liner replacement for the if-else statement. 
+It is used to write a conditional expression, where based on the result of a boolean condition, one of the two values is selected.
+Syntax:
+  condition ? exprIfTrue : exprIfFalse
+Note: The ternary operator takes a condition and returns one of two values, depending upon the condition’s boolean value, i.e., true or false.
+*/

85f163869258d8cf95c6f1f50b8bf90094bdea94
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-06 08:59:23 +0300

    question 2

--- Conditions/Questions/q2.dart
+++ Conditions/Questions/q2.dart
@@ -6,16 +6,24 @@ void main() {
   try {
     print("Enter a Vowel or a Consonant:");
     String? vowel = stdin.readLineSync();
-    if (vowel == 'a' ||
-        vowel == 'e' ||
-        vowel == 'i' ||
-        vowel == 'o' ||
-        vowel == 'u') {
-      print("Wow Vowel");
+    if (isVowel(vowel.toString())) {
+      print("$vowel wow is a Vowel");
     } else {
-      print("This is a consonant");
+      print("$vowel is a consonant");
     }
   } catch (ex) {
     print("Eter a Vowel Or an ALphabet Must be a string");
   }
+}
+
+bool isVowel(String char) {
+  // Convert the character to lowercase to handle both uppercase and lowercase inputs
+  char = char.toLowerCase();
+
+  // Check if the character is a vowel (a, e, i, o, u)
+  return char == 'a' ||
+      char == 'e' ||
+      char == 'i' ||
+      char == 'o' ||
+      char == 'u';
 }

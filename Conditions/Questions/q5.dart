1b89b5c64ec21653feb910ea61944bef082969c7
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-06 09:34:01 +0300

    question 5

---
+++ Conditions/Questions/q5.dart
@@ -0,0 +1,10 @@
+//Write a dart program to calculate the sum of natural numbers.
+
+void main() {
+  int sum = 0;
+  int n = 100;
+  for (int i = 0; i <= n; i++) {
+    sum = sum + i;
+  }
+  print("$sum This is the sum of Natural Numbers");
+}

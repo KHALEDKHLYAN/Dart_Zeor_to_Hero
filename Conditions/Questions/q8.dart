8adaff023dc51f879131b02ef454e4a5d54cf623
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-06 10:46:59 +0300

    question 8

--- Conditions/Questions/q8.dart
+++ Conditions/Questions/q8.dart
@@ -3,38 +3,42 @@
 import 'dart:io';
 
 void main() {
-  print("Enter a Number:");
-  double num1 = double.parse(stdin.readLineSync()!);
-  print("Enter Seccond Number:");
-  double num2 = double.parse(stdin.readLineSync()!);
+  try {
+    print("Enter a Number:");
+    double num1 = double.parse(stdin.readLineSync()!);
+    print("Enter Seccond Number:");
+    double num2 = double.parse(stdin.readLineSync()!);
 
-  print("Enter the operation(+,-,*,/):");
-  String? operation = stdin.readLineSync()!;
+    print("Enter the operation(+,-,*,/):");
+    String? operation = stdin.readLineSync()!;
 
-  double result;
+    double result;
 
-  switch (operation) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-      if (num2 != 0) {
-        result = num1 / num2;
-      } else {
-        print("Error: Cannot be divisible by zero");
+    switch (operation) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        if (num2 != 0) {
+          result = num1 / num2;
+        } else {
+          print("Error: Cannot be divisible by zero");
+          return;
+        }
+        break;
+      default:
+        print("Invalid operation");
         return;
-      }
-      break;
-    default:
-      print("Invalid operation");
-      return;
+    }
+    // Display the result
+    print("Result: $num1 $operation $num2 = $result");
+  } catch (ex) {
+    print("Enter Numbers only");
   }
-  // Display the result
-  print("Result: $num1 $operation $num2 = $result");
 }

80c1a9cab816f2e8344ea305206c4ffe419e4677
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-07 18:31:20 +0300

    Functions

--- functions/parameter.dart
+++ functions/parameter.dart
@@ -1,8 +1,9 @@
-void printInfo(String name, String gender) {
-  print("Hello $name your gender is $gender");
+void printInfo(String name, String gender, [String title = "Mr/Mss/Madaam"]) {
+  print("Hello $title $name your gender is $gender");
 }
 
 void main() {
   printInfo('khaled', "Male");
-  printInfo('male', "jane");
+  printInfo('Jane', "Female", 'mrs');
+  printInfo("John", 'Male', 'Mr');
 }

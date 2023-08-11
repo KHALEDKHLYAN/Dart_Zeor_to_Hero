0c081cce8d1d69ad9e8a9cc6c4431a548f8bbd32
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-11 22:57:39 +0300

    q2 Clollection

---
+++ collections/question/q2.dart
@@ -0,0 +1,9 @@
+//Create a set of fruits and print all fruits using loop.
+
+void main(){
+  Set<String> fruits = {"Bananas","Mangoes","Oranges","Strawberries"};
+
+  for(String fruit in fruits){
+    print(fruit);
+  }
+}

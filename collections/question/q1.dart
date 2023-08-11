b9bfec2efc38bedc3ae868c7f3c81d31c1b1e50a
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-11 22:53:24 +0300

    q1 Clollection

---
+++ collections/question/q1.dart
@@ -0,0 +1,8 @@
+//Create a list of names and print all names using list.
+
+void main(){
+  List<String> nameList = ['Joseph', 'John','keith','John','khaled'];
+  for(var i=0;i<=nameList.length-1 ;++i){
+    print("${nameList[i]}");
+  }
+}

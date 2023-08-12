a5590d0f5aff37536f54d2b94498ce7d6a009028
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-12 23:59:06 +0300

    ReadFile

--- fileHandling/readfile.dart
+++ fileHandling/readfile.dart
@@ -5,6 +5,9 @@ void main(){
   File file = File('text.txt');
 
   String contents = file.readAsStringSync();
-
   print(contents);
+  //get file location
+  print(file.path);
+  //get Absolute path
+  print(file.absolute.path);
 }

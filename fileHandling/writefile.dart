aafa95ba6e727b840ec13afdc83dfe790d8ad14e
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-13 04:43:13 +0300

    write file

--- fileHandling/writefile.dart
+++ fileHandling/writefile.dart
@@ -7,4 +7,9 @@ void main(){
 
   file.writeAsStringSync("Wow Awesome!");
   print('File written.');
+  print("\n");
+  //Adding new content to a file
+  file.writeAsStringSync("\n Today is on Sunday! Kindly Note!!", mode:FileMode.append);
+  print("Congratulations! Thank you for adding me up!");
 }
+//Note: If you have already some content in test.txt file, then it will be removed and replaced with new content.

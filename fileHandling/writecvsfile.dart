4b05332bfec322389052b7ab5a05501c07168d9d
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-13 05:00:43 +0300

    write csvfile

---
+++ fileHandling/writecvsfile.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(){
+  File file = File('students.csv');
+
+  file.writeAsStringSync("Name,Phone\n");
+  for(int i =0;i<3;i++){
+    stdout.write("Enter YourFirst Name ${i + 1}: ");
+    String? name = stdin.readLineSync();
+    stdout.write("Enter Your phoneNumber: ${i+1}");
+    String? phoneNumber = stdin.readLineSync();
+    file.writeAsStringSync("$name , $phoneNumber\n", mode: FileMode.append);
+  }
+  print("COngratulations! For adding To Csv!!");
+}

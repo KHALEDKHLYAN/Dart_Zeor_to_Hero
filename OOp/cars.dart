7c89b94b54e760f570bc0f3c325666eb866f3628
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-15 05:24:43 +0300

    car properties in oops

---
+++ OOp/cars.dart
@@ -0,0 +1,23 @@
+/*
+In this example below there is class Car with three properties: name, color, and numberOfSeats. 
+The class also has a method called start, which prints out the message “Car Started”. 
+We also have an object of the class Car called car.
+*/
+
+class Car{
+  String? name;
+  String? color;
+  int? numberOfSeats;
+
+  void start(){
+    print("$name Car started");
+  }
+}
+
+void main(){
+  Car car = Car();
+  car.name = "Buggati";
+  car.color= "GoldenOrage";
+  car.numberOfSeats= 8;
+  car.start();
+}

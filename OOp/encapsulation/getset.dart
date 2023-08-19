682016fb89e1d6a99945f3012796af324cc109ca
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-19 12:25:12 +0300

    get/set encapsulation

--- OOp/encapsulation/getset.dart
+++ OOp/encapsulation/getset.dart
@@ -7,28 +7,33 @@ The getter and setter methods are named model and year.
 The getter and setter methods are used to access and update the value of the private properties.
 */
 
-class Vehicle{
+class Vehicle {
   String? _model;
   int? _year;
 
-  //Getter method
+  // Getter method
   String get model => _model!;
 
   // Setter method
   set model(String model) => _model = model;
 
-  //Getter method
-  int get year=>_year!;
+  // Getter method
+  int get year => _year!;
 
-  //setter method
+  // Setter method
   set year(int year) => _year = year;
-
 }
 
-void main(){
-  var vehicl1 = Vehicle();
-  vehicl1.model = "Toyota";
-  vehicl1.year = 2000;
-
-  print("Your kid selected a${vehicl1.model} and for ${vehicl1.year}");
+void main() {
+  var vehicle = Vehicle();
+  vehicle.model = "Toyota";
+  vehicle.year = 2019;
+  print(vehicle.model);
+  print(vehicle.year);
+  
+  var vehicle2 = Vehicle();
+  vehicle2.model = "Toyota";
+  vehicle2.year = 2019;
+  print(vehicle2.model);
+  print(vehicle2.year);
 }

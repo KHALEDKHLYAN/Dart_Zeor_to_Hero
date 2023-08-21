aa3909c543ff51ebb28eb2f5b62c090a45d33a1d
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-21 20:01:10 +0300

    set std

--- OOp/encapsulation/stdset.dart
+++ OOp/encapsulation/stdset.dart
@@ -35,3 +35,10 @@ void main() {
   student.classnumber = 14;
   student.display();
 }
+
+/*
+Why Is Setter Important?
+    It is used to set the value of a private property.
+    It is also used for data validation.
+    It gives you better control over the data.
+*/

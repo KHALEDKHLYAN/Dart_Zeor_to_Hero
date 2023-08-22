3f5c6e45197d9c8339bd8eb1f19e03ec75daa2f7
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-22 22:43:04 +0300

    getter and Setter BankAccount

--- OOp/encapsulation/bankaccount.dart
+++ OOp/encapsulation/bankaccount.dart
@@ -32,3 +32,11 @@ void main(){
   account.withdraw(3000);
   print("Balance : ${account.balance}");
 }
+
+/*
+When To Use Getter And Setter
+Use getter and setter when you want to restrict the access to the properties.
+Use getter and setter when you want to perform some action before reading or writing the properties.
+Use getter and setter when you want to validate the data before reading or writing the properties.
+Don’t use getter and setter when you want to make the properties read-only or write-only.
+*/

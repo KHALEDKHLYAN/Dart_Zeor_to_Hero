1186f16a2d6a3c720e6119be4a1615d7c7229e35
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-23 21:18:52 +0300

    inheritance in dart

--- OOp/inheritance/inherit.dart
+++ OOp/inheritance/inherit.dart
@@ -63,3 +63,12 @@ void main(){
   student.displaySchoolInfo();
 
 }
+
+/*
+Advantages Of Inheritance In Dart
+It promotes reusability of the code and reduces redundant code.
+It helps to design a program in a better way.
+It makes code simpler, cleaner and saves time and money on maintenance.
+It facilitates the creation of class libraries.
+It can be used to enforce standard interface to all children classes.
+*/

c32878c2510f0df2fc6445016f49f1c3a565292b
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-25 23:03:55 +0300

    polymophysicm

--- OOp/polymophysim/employee.dart
+++ OOp/polymophysim/employee.dart
@@ -33,3 +33,10 @@ void main(){
   employee.salary();
   developer.salary();
 }
+
+
+/*
+Advantage Of Polymorphism In Dart
+Subclasses can override the behavior of the parent class.
+It allows us to write code that is more flexible and reusable.
+*/

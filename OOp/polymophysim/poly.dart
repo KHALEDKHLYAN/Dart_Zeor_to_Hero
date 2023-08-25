a032c97441c17ded91038a3dcb78ac7682a50346
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-25 21:23:17 +0300

    polymophysicm

---
+++ OOp/polymophysim/poly.dart
@@ -0,0 +1,42 @@
+/*
+Poly - Many
+Morph - Forms
+
+Polymorphism -- is the ability of an object to take on many forms.
+
+In real world, Polymorphism is updating or modyfying the feature, function, 
+implementation that already  that already exist in the parent class
+
+Method overriding is a trchnique in which you can create a method in child class that has the same name as the method in parent class
+the method in the parent class
+
+
+Syntax
+Class Parent{
+  void Function{
+  }
+}
+class child extends ParentClass{
+  @override.
+  Void Function(){}
+}
+*/
+
+
+class Animal{
+  eat(){
+    print("Animal is Eating");
+  }
+}
+
+class Dog extends Animal{
+  @override
+  eat(){
+    print("Dog is eating");
+  }
+}
+
+void main(){
+  Animal animal = Animal();
+  animal.eat();
+}

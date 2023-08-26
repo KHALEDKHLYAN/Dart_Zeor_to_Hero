b9e61895e109a00578397ca64e58c41db6a19671
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-26 22:49:33 +0300

    static in dart

---
+++ OOp/static/static.dart
@@ -0,0 +1,51 @@
+/*
+STATIC IN DART
+
+Dart Static Variable
+A static variable is a variable that is shared by all instances of a class. 
+It is declared using the static keyword. It is initialized only once when the class is loaded. 
+It is used to store the class-level data.
+
+Static variables are declared outside of any function. 
+They can be accessed from anywhere in the program, and they persist even after a user closes their browser
+
+class ClassName {
+  static dataType variableName = value;
+  // Accessing the static variable inside same class
+  void display() {
+    print(variableName);
+  }
+}
+
+void main() {
+  // Accessing static variable outside the class
+  dataType value =ClassName.variableName;
+}
+
+Example 1: Static Variable In Dart
+In this example below, there is a class named Employee. 
+The class has a static variable count to count the number of employees.
+*/
+
+class Employee{
+  //static dataType
+  static int count=0;
+
+  //constructor
+  Employee(){
+    count++;
+  }
+
+  display(){
+    print("Total EMployee ${count}");
+  }
+}
+
+void main(){
+  Employee em = Employee();
+  em.display();
+  Employee em2 = Employee();
+  em2.display();
+  Employee em3 = Employee();
+  em3.display();
+}

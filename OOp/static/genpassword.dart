263abbce0b439c31c9607ce896026263e411811d
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-27 16:46:10 +0300

    dart static methods

---
+++ OOp/static/genpassword.dart
@@ -0,0 +1,27 @@
+/*
+Example 4: Static Method In Dart
+In this example below, there is static method generateRandomPassword() which generates a random password. 
+You can call PasswordGenerator.generateRandomPassword() anytime without creating an instance of the class.
+
+*/
+import 'dart:math';
+
+class PasswordGenerator {
+  static String generateRandomPassword() {
+    List<String> allalphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    List<int> numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    List<String> specialCharacters = ["@", "#", "%", "&", "*"];
+    List<String> password = [];
+    for (int i = 0; i < 5; i++) {
+      password.add(allalphabets[Random().nextInt(allalphabets.length)]);
+      password.add(numbers[Random().nextInt(numbers.length)].toString());
+      password
+          .add(specialCharacters[Random().nextInt(specialCharacters.length)]);
+    }
+    return password.join();
+  }
+}
+
+void main() {
+  print(PasswordGenerator.generateRandomPassword());
+}

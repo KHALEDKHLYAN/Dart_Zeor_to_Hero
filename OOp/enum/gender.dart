0af54dc2e123032520f585b4713aa2fdf581cdae
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-08-28 03:55:56 +0300

    dart Enum

---
+++ OOp/enum/gender.dart
@@ -0,0 +1,33 @@
+/*
+In this example, there is an enum type named Gender. 
+It contains three constants Male, Female, and Other. 
+The Gender enum type is used in the Person class.
+*/
+
+
+enum Gender{
+  Male,
+  Female,
+  Transgender
+}
+
+class Student{
+  String? firstName;
+  String? lastName;
+  Gender gender;
+
+  //Construcor
+  Student(this.firstName, this.lastName, this.gender);
+
+  // method to display
+  display(){
+    print("Students FullName : ${firstName}''${lastName}");
+    print("Student Gender: ${gender}");
+  }
+}
+
+void main(){
+  // Creating a new object of student Class with constructor parameters
+  Student student = Student("Keisha", "Maryana", Gender.Female);
+  student.display();
+}

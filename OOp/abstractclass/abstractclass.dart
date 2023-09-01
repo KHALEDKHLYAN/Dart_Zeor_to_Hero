9b5556359565419c672b028da9d7b3c88c3817d6
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-09-01 22:52:05 +0300

    abstract class

---
+++ OOp/abstractclass/abstractclass.dart
@@ -0,0 +1,17 @@
+/*
+
+In this section, you will learn about dart abstract class. 
+You can create an object of concrete classes, but you cannot create an object of abstract classes.
+
+Abstract classes are classes that cannot be initialized. 
+It is used to define the behavior of a class that can be inherited by other classes. 
+An abstract class is declared using the keyword abstract.
+
+syntax
+abstract class ClassName {
+  //Body of abstract class
+
+  method1();
+  method2();
+}
+*/

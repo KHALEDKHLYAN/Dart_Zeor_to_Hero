9f0a89321e214f734153d3914f547244e6847af8
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-09-03 21:07:28 +0300

    interface in dart

--- OOp/abstractclass/bank.dart
+++ OOp/abstractclass/bank.dart
@@ -55,3 +55,10 @@ void main(){
   sbi.display();
 }
 
+/*
+Key Points To Remember
+You can’t create an object of an abstract class.
+It can have both abstract and non-abstract methods.
+It is used to define the behavior of a class that other classes can inherit.
+Abstract method only has a signature and no implementation.
+*/
---
+++ OOp/interface/interface.dart
@@ -0,0 +1,29 @@
+/*
+An interface defines a syntax that a class must follow.
+
+It is a contract that defines the capabilities of a class. 
+It is used to achieve abstraction in the Dart programming language. 
+When you implement an interface, you must implement all the properties and methods defined in the interface. 
+Keyword implements is used to implement an interface.
+Syntax Of Interface In Dart
+class InterfaceName {
+  // code
+}
+
+class ClassName implements InterfaceName {
+  // code
+}
+
+In dart there is no keyword interface but you can use class or abstract class to declare an interface. 
+All classes implicitly define an interface. 
+Mostly abstract class is used to declare an interface.
+// creating an interface using abstract class
+abstract class Person {
+  canWalk();
+  canRun();
+}
+
+In this example below, there is an interface Laptop with two methods turnOn() and turnOff(). 
+The class MacBook implements the interface and overrides the metIn this example below, there is an interface Laptop with two methods turnOn() and turnOff(). The class MacBook implements the interface and overrides the methods to print the message.hods to print the message.
+*/
+

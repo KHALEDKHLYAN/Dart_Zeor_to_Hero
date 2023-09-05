765b66ac1ba74c2627a45b40fa469fde4c07ce12
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-09-05 21:13:42 +0300

    Mixin oops dart

---
+++ OOp/mixin/onmixin.dart
@@ -0,0 +1,46 @@
+/*
+On Keyword
+Sometimes, you want to use a mixin only with a specific class. In this case, you can use the on keyword.
+
+   Syntax Of On Keyword
+
+    mixin Mixin1 on Class1{
+      // code
+    }
+
+
+    Example 3: On Keyword In Mixin In Dart
+In this example below, there is abstract class named Animal with properties name and speed. 
+The Animal class has an abstract method run(). 
+The CanRun mixin is only used by class that extends Animal. 
+The Dog class extends the Animal class and uses the CanRun mixin. 
+The Bird class cannot use the CanRun mixin because it does not extend the Animal class.
+*/
+
+abstract class Animal{
+  //Properties
+  String? name;
+  double? speed;
+
+  //Constructor
+  Animal(this.name, this.speed);
+
+  //abstract method
+  void run();
+}
+
+mixin CanRun on Animal{
+  //method
+  void run() => print("$name can run with a Speed of: $speed H/Km ");
+}
+
+class Dog extends Animal with CanRun{
+
+  //Constructor
+  Dog(String name, double speed): super(name, speed);
+}
+
+void main(){
+  Dog dog = Dog("german Shepherd", 230);
+  dog.run();
+}

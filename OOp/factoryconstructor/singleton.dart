be228e4d1da57e8c26770d83dae5af6de5f8ae09
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-09-07 22:09:53 +0300

    singleton in factory

--- OOp/factoryconstructor/singleton.dart
+++ OOp/factoryconstructor/singleton.dart
@@ -32,4 +32,19 @@ void main() {
  print(obj1.hashCode);
  print(obj2.hashCode);
 }
- 
+
+
+/*
+You can see that both objects have the same hashcode. This is because both objects are pointing to the same instance.
+Note: Here Singleton._internal() is a private constructor so that it can not be called from outside the library. 
+      The factory constructor is used to return the same instance of the class.
+
+Key Points
+Here It means factory constructor
+
+It uses the factory keyword to define a factory constructor.
+It returns an instance of the same class or sub-class.
+It is used to implement factory design patterns. [Return sub-class instance based on input parameter as shown in example 4]
+It is used to implement singleton design patterns. [Return the same instance every time]
+It is used to initialize a final variable using logic that can’t be handled in the initializer list.
+*/

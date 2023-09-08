839c81e154426bf044b0216f94bc5887b3c2f6d3
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-09-08 23:35:04 +0300

    Generic data

---
+++ OOp/generic/map.dart
@@ -0,0 +1,11 @@
+/*
+Dart Map Class
+    Like List, internal implementation of Map work with different types 
+    of data like int, String, double, etc. 
+    This is because Map is a generic class.
+  
+  abstract class Map<K, V> {
+  // code
+  external factory Map();
+}
+*/

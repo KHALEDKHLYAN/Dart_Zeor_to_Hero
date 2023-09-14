e44cca9bc3c0c40618820ad937c95b80867c7cd3
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-09-14 20:50:29 +0300

    Question 6

--- OOp/Questions/q6.dart
+++ OOp/Questions/q6.dart
@@ -5,3 +5,31 @@ Add a factory constructor to Bottle and return the object of CokeBottle.
 Instantiate CokeBottle using the factory constructor and call the open() on the object.
 */
 
+// Define the Bottle interface
+abstract class Bottle {
+  void open();
+}
+
+// Implement the Bottle interface with CokeBottle
+class CokeBottle implements Bottle {
+  @override
+  void open() {
+    print("Coke bottle is opened");
+  }
+}
+
+// Add a factory constructor to Bottle
+class BottleFactory {
+  static Bottle create() {
+    return CokeBottle();
+  }
+}
+
+void main() {
+  // Instantiate CokeBottle using the factory constructor
+  Bottle bottle = BottleFactory.create();
+
+  // Call the open method on the object
+  bottle.open(); // Output: "Coke bottle is opened"
+}
+

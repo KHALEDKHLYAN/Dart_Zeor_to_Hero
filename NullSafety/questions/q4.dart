6c36ec5288ed2efbb8b7aff631ec8959c0443038
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-09-18 08:58:52 +0300

    Null Safety q4

---
+++ NullSafety/questions/q4.dart
@@ -0,0 +1,10 @@
+//Write a program in a dart to create an age variable and assign a null value to it using ?.
+void main() {
+  int? age; // Declaring 'age' as nullable using int?
+
+  // Assigning null to the 'age' variable
+  age = null;
+
+  // Printing the value of 'age'
+  print("Age: $age");
+}

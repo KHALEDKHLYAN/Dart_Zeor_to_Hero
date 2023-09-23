b2232e8a1a71756b101745c01c370dbb2d277c4d
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-09-23 05:56:53 +0300

    Future COmmit

--- Asynchronous/future.dart
+++ Asynchronous/future.dart
@@ -17,3 +17,19 @@ void main(){
   getUserName().then((value) => print(value));
   print('End!! Sir');
 }
+
+/*
+Future represents the result of an asynchronous operation and can have 2 states.
+
+State Of Future
+    Uncompleted
+    Completed
+Uncompleted
+  When you call an asynchronous function, it returns to an uncompleted future. It means the future is waiting for the function asynchronous operation to finish or to throw an error.
+Completed
+It can be completed with value or completed with error. 
+Future<int> produces an int value, and Future<String> produces a String value. 
+If the future doesn’t produce any value, then the type of future is Future<void>.
+
+Note: If the asynchronous operation performed by the function fails due to any reason, the future completes with an error.
+*/

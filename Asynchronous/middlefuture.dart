e74799a63d0a71ac5c31db04f02604eea536d607
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-09-23 06:21:11 +0300

    Future COmmit

---
+++ Asynchronous/middlefuture.dart
@@ -0,0 +1,21 @@
+/*
+In this example below, we are creating a function middleFunction() that returns a future. 
+The function will return Future<String> after 5 seconds.
+*/
+
+void main(){
+  print('Start');
+  getData();
+  print('ENd');
+
+}
+
+void getData()async{
+  String data = await middleFunction();
+  print(data);
+}
+
+Future<String> middleFunction(){
+  //return new Future();
+  return Future.delayed(Duration(seconds: 2),()=> 'Hello');
+}
